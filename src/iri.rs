52dfa59cb14bcc2b532dad09c8ffc3a4eb9af9c4
Timothée Haudebourg <timothee.haudebourg@irisa.fr>
2020-03-07 16:36:18 +0100

    It compiles.

--- src/iri.rs
+++ src/iri.rs
@@ -1,89 +1,155 @@
 use crate::utf8;
 
 #[derive(Debug)]
-enum Error {
+pub enum Error {
 	/// The input data is not a valid UTF-8 encoded string.
 	InvalidEncoding,
 
 	Invalid,
 
-	InvalidData
+	InvalidPath,
+
+	InvalidPCTEncoded
 }
 
-struct Authority {
+struct AuthorityData {
 	offset: usize,
 	userinfo_len: Option<usize>,
 	host_len: Option<usize>,
 	port_len: Option<usize>
 }
 
-struct Iri<'a> {
+impl AuthorityData {
+	pub fn is_empty(&self) -> bool {
+		self.userinfo_len.is_none() && self.host_len.is_none() && self.port_len.is_none()
+	}
+
+	pub fn len(&self) -> usize {
+		let mut len = 0;
+
+		if let Some(l) = self.userinfo_len {
+			len += l + 1;
+		}
+
+		if let Some(l) = self.host_len {
+			len += l;
+		}
+
+		if let Some(l) = self.port_len {
+			len += 1 + l;
+		}
+
+		len
+	}
+
+	pub fn host_offset(&self) -> usize {
+		let mut offset = self.offset;
+
+		if let Some(l) = self.userinfo_len {
+			offset += l + 1;
+		}
+
+		offset
+	}
+
+	// pub fn port_offset(&self) -> usize {
+	// 	let mut offset = self.offset;
+	//
+	// 	if let Some(l) = self.userinfo_len {
+	// 		offset += l + 1;
+	// 	}
+	//
+	// 	if let Some(l) = self.host_len {
+	// 		offset += l;
+	// 	}
+	//
+	// 	if let Some(_) = self.port_len {
+	// 		offset += 1;
+	// 	}
+	//
+	// 	offset
+	// }
+}
+
+pub struct Iri<'a> {
 	data: &'a [u8],
 	scheme_len: usize,
-	authority: Authority,
+	authority: AuthorityData,
 	path_len: usize
 }
 
+pub struct Authority<'a> {
+	data: &'a [u8],
+	authority: &'a AuthorityData
+}
+
 impl<'a> Iri<'a> {
-	pub fn new<S: AsRef<[u8]>>(buffer: &'a [u8]) -> Result<Iri<'a>> {
+	pub fn new<S: AsRef<[u8]> + ?Sized>(buffer: &'a S) -> Result<Iri<'a>, Error> {
+		let buffer = buffer.as_ref();
 		let scheme_len = parse_scheme(buffer, 0)?;
-		let mut authority_offset = 0;
-		let mut authority_len = 0;
-		let mut path_len = 0;
-		expect(buffer, ':')?;
+		let mut authority = AuthorityData {
+			offset: 0,
+			userinfo_len: None,
+			host_len: None,
+			port_len: None
+		};
+		let path_len;
+		expect(buffer, ':', scheme_len)?;
 
 		match get_char(buffer, scheme_len + 1)? {
 			Some(('/', 1)) => {
 				match get_char(buffer, scheme_len + 2)? {
 					Some(('/', 1)) => {
-						authority_offset = scheme_len + 3;
-						let authority_end = parse_authority(buffer, authority_offset)?;
+						authority.offset = scheme_len + 3;
+						authority = parse_authority(buffer, authority.offset)?;
+						let authority_end = authority.offset + authority.len();
 						let path_end = parse_path(buffer, authority_end, true, false)?;
 
-						authority_len = authority_end - authority_offset;
+						// let authority_len = authority_end - authority.offset;
 						path_len = path_end - authority_end;
 					},
 					_ => {
-						authority_offset = scheme_len + 2;
-						let path_end = parse_path(buffer, authority_offset, true, true)?;
-						path_len = authority_end - authority_offset;
+						authority.offset = scheme_len + 2;
+						let path_end = parse_path(buffer, authority.offset, true, true)?;
+						path_len = path_end - authority.offset;
 					}
 				}
 			},
 			_ => {
-				authority_offset = scheme_len + 1;
-				let path_end = parse_path(buffer, authority_offset, false, true)?;
-				path_len = authority_end - authority_offset;
+				authority.offset = scheme_len + 1;
+				let path_end = parse_path(buffer, authority.offset, false, true)?;
+				path_len = path_end - authority.offset;
 			}
 		}
 
 		Ok(Iri {
 			data: buffer,
-			scheme_len, authority_offset, authority_len, path_len
+			scheme_len, authority, path_len
 		})
 	}
 
 	pub fn scheme(&self) -> &str {
 		unsafe {
-			std::str::from_utf8_unchecked(data[0..self.scheme_len])
+			std::str::from_utf8_unchecked(&self.data[0..self.scheme_len])
 		}
 	}
 
-	pub fn authority(&self) -> Option<&str> {
-		if self.authority_len > 0 {
-			unsafe {
-				Ok(std::str::from_utf8_unchecked(data[self.authority_offset..(self.authority_offset+self.scheme_len)]))
-			}
-		} else {
+	pub fn authority(&'a self) -> Option<Authority<'a>> {
+		if self.authority.is_empty() {
 			None
+		} else {
+			Some(Authority {
+				data: self.data,
+				authority: &self.authority
+			})
 		}
 	}
 
 	pub fn path(&self) -> Option<&str> {
 		if self.path_len > 0 {
 			unsafe {
-				let offset = self.authority_offset + self.authority_len;
-				Ok(std::str::from_utf8_unchecked(data[offset..(offset+self.path_len)]))
+				let offset = self.authority.offset + self.authority.len();
+				Some(std::str::from_utf8_unchecked(&self.data[offset..(offset+self.path_len)]))
 			}
 		} else {
 			None
@@ -91,14 +157,28 @@ impl<'a> Iri<'a> {
 	}
 }
 
-pub fn get_char(buffer: &[u8], i: usize) -> Result<usize, Error> {
+impl<'a> Authority<'a> {
+	pub fn host(&self) -> Option<&str> {
+		if let Some(len) = self.authority.host_len {
+			unsafe {
+				let offset = self.authority.host_offset();
+				Some(std::str::from_utf8_unchecked(&self.data[offset..(offset+len)]))
+			}
+		} else {
+			None
+		}
+	}
+}
+
+fn get_char(buffer: &[u8], i: usize) -> Result<Option<(char, usize)>, Error> {
 	match utf8::get_char(buffer, i) {
-		Ok(r) => r,
+		Ok(None) => Ok(None),
+		Ok(Some((c, len))) => Ok(Some((c, len as usize))),
 		Err(_) => Err(Error::InvalidEncoding)
 	}
 }
 
-pub fn expect(buffer: &[u8], c: char, i: usize) -> Result<(), Error> {
+fn expect(buffer: &[u8], c: char, i: usize) -> Result<(), Error> {
 	match get_char(buffer, i)? {
 		Some((found_c, 1)) => {
 			if found_c == c {
@@ -107,12 +187,12 @@ pub fn expect(buffer: &[u8], c: char, i: usize) -> Result<(), Error> {
 				Err(Error::Invalid)
 			}
 		},
-		None => Err(Error::Invalid)
+		_ => Err(Error::Invalid)
 	}
 }
 
 /// Parse the IRI scheme.
-pub fn parse_scheme(buffer: &[u8], mut i: usize) -> Result<usize, Error> {
+fn parse_scheme(buffer: &[u8], mut i: usize) -> Result<usize, Error> {
 	loop {
 		match get_char(buffer, i)? {
 			Some((c, len)) if (i == 0 && c.is_alphabetic()) || (i > 0 && (c.is_alphanumeric() || c == '+' || c == '-' || c == '.' || c == ':')) => {
@@ -125,7 +205,7 @@ pub fn parse_scheme(buffer: &[u8], mut i: usize) -> Result<usize, Error> {
 	Ok(i)
 }
 
-pub fn is_ucschar(c: char) -> bool {
+fn is_ucschar(c: char) -> bool {
 	let c = c as u32;
 	c >= 0xA0 && c <= 0xD7FF || c >= 0xF900 && c <= 0xFDCF || c >= 0xFDF0 && c <= 0xFFEF ||
 	c >= 0x10000 && c <= 0x1FFFD ||
@@ -144,47 +224,47 @@ pub fn is_ucschar(c: char) -> bool {
 	c >= 0xE1000 && c <= 0xEFFFD
 }
 
-pub fn is_unreserved(c: char) -> bool {
+fn is_unreserved(c: char) -> bool {
 	c.is_alphanumeric() || c == '-' || c == '.' || c == '_' || c == '~' || is_ucschar(c)
 }
 
-pub fn is_subdelim(c: char) -> bool {
+fn is_subdelim(c: char) -> bool {
 	match c {
 		'!' | '$' | '&' | '\'' | '(' | ')' | '*' | '+' | ',' | ';' | '=' => true,
 		_ => false
 	}
 }
 
-pub fn is_hex_digit(buffer: &[u8], mut i: usize) -> bool {
+fn is_hex_digit(buffer: &[u8], i: usize) -> Result<bool, Error> {
 	match get_char(buffer, i)? {
 		Some((c, 1)) => {
-			c.to_digit(16).is_some()
+			Ok(c.to_digit(16).is_some())
 		},
-		None => false
+		_ => Ok(false)
 	}
 }
 
-pub fn parse_pct_encoded(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
+fn parse_pct_encoded(buffer: &[u8], i: usize) -> Result<Option<usize>, Error> {
 	match get_char(buffer, i)? {
 		Some(('%', 1)) => {
-			if is_hex_digit(buffer, i+1) && is_hex_digit(buffer, i+2) {
+			if is_hex_digit(buffer, i+1)? && is_hex_digit(buffer, i+2)? {
 				Ok(Some(3))
 			} else {
 				Err(Error::InvalidPCTEncoded)
 			}
 		},
-		None => Ok(None)
+		_ => Ok(None)
 	}
 }
 
-pub fn parse_userinfo(buffer, &[u8], mut i: usize) -> Result<Option<usize>, Error> {
+fn parse_userinfo(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
 	loop {
 		match get_char(buffer, i)? {
 			Some(('@', 1)) => {
 				return Ok(Some(i))
 			},
 			Some(('%', 1)) => {
-				if let Some(len) = parse_pct_encoded(buffer, i) {
+				if let Some(len) = parse_pct_encoded(buffer, i)? {
 					i += len
 				} else {
 					break
@@ -200,9 +280,9 @@ pub fn parse_userinfo(buffer, &[u8], mut i: usize) -> Result<Option<usize>, Erro
 	Ok(None)
 }
 
-pub fn parse_dec_octet(buffer: &[u8], i: usize) -> Result<Option<(u32, usize)>, Error> {
-	let octet = 0u32;
-	let len = 0;
+fn parse_dec_octet(buffer: &[u8], i: usize) -> Result<Option<(u32, usize)>, Error> {
+	let mut octet = 0u32;
+	let mut len = 0;
 
 	loop {
 		match get_char(buffer, i + len)? {
@@ -235,23 +315,23 @@ pub fn parse_dec_octet(buffer: &[u8], i: usize) -> Result<Option<(u32, usize)>, 
 }
 
 /// Parse an IPv4 literal.
-pub fn parse_ipv4_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u32, usize)>> {
+fn parse_ipv4_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u32, usize)>, Error> {
 	let offset = i;
 	if let Some((a, olen)) = parse_dec_octet(buffer, i)? {
 		i += olen;
 		if let Some(('.', 1)) = get_char(buffer, i)? {
 			i += 1;
-			if let Some((a, olen)) = parse_dec_octet(buffer, i)? {
+			if let Some((b, olen)) = parse_dec_octet(buffer, i)? {
 				i += olen;
 				if let Some(('.', 1)) = get_char(buffer, i)? {
 					i += 1;
-					if let Some((a, olen)) = parse_dec_octet(buffer, i)? {
+					if let Some((c, olen)) = parse_dec_octet(buffer, i)? {
 						i += olen;
 						if let Some(('.', 1)) = get_char(buffer, i)? {
 							i += 1;
-							if let Some((a, olen)) = parse_dec_octet(buffer, i)? {
+							if let Some((d, olen)) = parse_dec_octet(buffer, i)? {
 								i += olen;
-								let ipv4 = (a << 24) | (b << 16) | (c << 8) | d
+								let ipv4 = (a << 24) | (b << 16) | (c << 8) | d;
 								let len = i - offset;
 								return Ok(Some((ipv4, len)))
 							}
@@ -265,15 +345,15 @@ pub fn parse_ipv4_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u32, us
 	Ok(None)
 }
 
-pub fn parse_h16(buffer: &[u8], mut i: usize) -> Result<Option<(u16, usize)>, Error> {
-	let len = 0;
-	let h16 = 0;
+fn parse_h16(buffer: &[u8], i: usize) -> Result<Option<(u16, usize)>, Error> {
+	let mut len = 0;
+	let mut h16 = 0;
 
 	loop {
 		match get_char(buffer, i + len)? {
 			Some((c, 1)) => {
 				if let Some(d) = c.to_digit(16) {
-					h16 = (h16 << 4) | d;
+					h16 = (h16 << 4) | d as u16;
 					len += 1;
 					if len >= 4 {
 						break
@@ -291,14 +371,14 @@ pub fn parse_h16(buffer: &[u8], mut i: usize) -> Result<Option<(u16, usize)>, Er
 
 /// Parse an IPv6 literal.
 /// Return the IPv6 and the string length.
-pub fn parse_ipv6_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u128, usize)>, Error> {
+fn parse_ipv6_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u128, usize)>, Error> {
 	let mut lhs = 0u128;
 	let mut lhs_count = 0;
 
 	let mut lit = 0u128;
 	let mut lit_count = 0;
 
-	let is_lhs = true;
+	let mut is_lhs = true;
 	let offset = i;
 
 	loop {
@@ -307,11 +387,11 @@ pub fn parse_ipv6_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u128, u
 		}
 
 		if is_lhs {
-			if let Some((':', 1)) = get_char(buffer, i) {
+			if let Some((':', 1)) = get_char(buffer, i)? {
 				i += 1;
 
 				if lhs_count == 0 {
-					if let Some((':', 1)) = get_char(buffer, i) {
+					if let Some((':', 1)) = get_char(buffer, i)? {
 						i += 1;
 					} else {
 						return Ok(None); // Invalid IPv6 (unexpected char)
@@ -330,20 +410,20 @@ pub fn parse_ipv6_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u128, u
 		}
 
 		if lhs_count + lit_count <= 6 {
-			if let Some((ipv4, len)) = parse_ipv4_literal(buffer, i) {
-				lit = (lit << 32) | n;
-				lit_count += 2;
+			if let Some((ipv4, len)) = parse_ipv4_literal(buffer, i)? {
+				lit = (lit << 32) | ipv4 as u128;
+				// lit_count += 2;
 				i += len;
 				break
 			}
 		}
 
-		if let Some((n, len)) = parse_h16(buffer, i) {
-			lit = (lit << 16) | n;
+		if let Some((n, len)) = parse_h16(buffer, i)? {
+			lit = (lit << 16) | n as u128;
 			lit_count += 1;
 			i += len;
 
-			match get_char(buffer, i) {
+			match get_char(buffer, i)? {
 				Some((']', 1)) => {
 					break
 				},
@@ -365,7 +445,7 @@ pub fn parse_ipv6_literal(buffer: &[u8], mut i: usize) -> Result<Option<(u128, u
 	Ok(Some((lit, len)))
 }
 
-pub fn parse_ip_literal(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
+fn parse_ip_literal(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
 	let offset = i;
 	if let Some(('[', 1)) = get_char(buffer, i)? {
 		i += 1;
@@ -385,7 +465,7 @@ pub fn parse_ip_literal(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Er
 	Ok(None)
 }
 
-pub fn parse_ireg_name(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
+fn parse_ireg_name(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
 	let offset = i;
 	loop {
 		match get_char(buffer, i)? {
@@ -412,23 +492,23 @@ pub fn parse_ireg_name(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Err
 	}
 }
 
-pub fn parse_host(buffer: &[u8], i: usize) -> Result<usize, Error> {
-	if let Some(len) = parse_ip_literal(buffer, i) {
-		Ok(len)
-	} else if let Some(len) = parse_ipv4_literal(buffer, i) {
-		Ok(len)
+fn parse_host(buffer: &[u8], i: usize) -> Result<Option<usize>, Error> {
+	if let Some(len) = parse_ip_literal(buffer, i)? {
+		Ok(Some(len))
+	} else if let Some((_, len)) = parse_ipv4_literal(buffer, i)? {
+		Ok(Some(len))
 	} else {
 		parse_ireg_name(buffer, i)
 	}
 }
 
-pub fn parse_port(buffer: &[u8], mut i: usize) -> Result<usize, Error> {
+fn parse_port(buffer: &[u8], mut i: usize) -> Result<Option<usize>, Error> {
 	let offset = i;
 	loop {
 		match get_char(buffer, i)? {
 			Some((c, 1)) => {
-				if let Some(d) = c.to_digit(10) {
-					i += len
+				if let Some(_) = c.to_digit(10) {
+					i += 1
 				} else {
 					break
 				}
@@ -447,11 +527,11 @@ pub fn parse_port(buffer: &[u8], mut i: usize) -> Result<usize, Error> {
 }
 
 /// Parse the IRI authority.
-pub fn parse_authority(buffer: &[u8], mut i: usize) -> Result<Authority, Error> {
+fn parse_authority(buffer: &[u8], mut i: usize) -> Result<AuthorityData, Error> {
 	let offset = i;
 	let mut userinfo_len = None;
 	let mut host_len = None;
-	let mut port_len;
+	let port_len;
 
 	if let Some(len) = parse_userinfo(buffer, i)? {
 		userinfo_len = Some(len);
@@ -467,37 +547,39 @@ pub fn parse_authority(buffer: &[u8], mut i: usize) -> Result<Authority, Error> 
 		Some((':', 1)) => {
 			i += 1;
 			if let Some(len) = parse_port(buffer, i)? {
-				i += len;
+				// i += len;
 				Some(len)
 			} else {
 				Some(0)
 			}
 		},
 		_ => None
-	}
+	};
 
-	Ok(Authority {
+	Ok(AuthorityData {
 		offset, userinfo_len, host_len, port_len
 	})
 }
 
 /// Parse IRI path.
-pub fn parse_path(buffer: &[u8], mut i: usize, mut absolute: bool, non_empty: bool) -> Result<usize, Error> {
+fn parse_path(buffer: &[u8], mut i: usize, mut absolute: bool, non_empty: bool) -> Result<usize, Error> {
 	let start = i;
 
-	match get_char(buffer, i)? {
-		None | Some(('?', _)) | Some(('#', _)) => break,
-		Some(('/', len)) if i == start => {
-			if absolute {
-				return Err(Error::InvalidPath);
-			} else {
-				absolute = true;
-			}
+	loop {
+		match get_char(buffer, i)? {
+			None | Some(('?', _)) | Some(('#', _)) => break,
+			Some(('/', len)) if i == start => {
+				if absolute {
+					return Err(Error::InvalidPath);
+				} else {
+					absolute = true;
+				}
 
-			i += len
-		},
-		Some((_, len)) => {
-			i += len
+				i += len
+			},
+			Some((_, len)) => {
+				i += len
+			}
 		}
 	}
 
--- src/utf8.rs
+++ src/utf8.rs
@@ -1,32 +1,38 @@
 pub fn get_byte(buffer: &[u8], i: usize) -> Option<u32> {
 	match buffer.get(i) {
-		Some(c) => Some(c as u32),
+		Some(c) => Some(*c as u32),
 		None => None
 	}
+}
+
+pub fn expect_byte(buffer: &[u8], i: usize) -> Result<u32, ()> {
+	get_byte(buffer, i).ok_or(())
 }
 
 /// Return a char and the size of its UTF-8 encoding.
 pub fn get_codepoint(buffer: &[u8], i: usize) -> Result<Option<(u32, u8)>, ()> {
 	if let Some(a) = get_byte(buffer, i) {
-		if a & 0x80 == 0x00 {
-	        Ok((a, 1))
+		let r = if a & 0x80 == 0x00 {
+	        (a, 1)
 	    } else if a & 0xE0 == 0xC0 {
-	        let b = get_byte(buffer, i + 1)?;
-	        Ok(((a & 0x1F) << 6 | b, 2))
+	        let b = expect_byte(buffer, i + 1)?;
+	        ((a & 0x1F) << 6 | b, 2)
 	    } else if a & 0xF0 == 0xE0 {
-	        let b = get_byte(buffer, i + 1)?;
-	        let c = get_byte(buffer, i + 2)?;
-	        Ok(((a & 0x0F) << 12 | b << 6 | c, 2))
+	        let b = expect_byte(buffer, i + 1)?;
+	        let c = expect_byte(buffer, i + 2)?;
+	        ((a & 0x0F) << 12 | b << 6 | c, 2)
 	    } else if a & 0xF8 == 0xF0 {
-	        let b = get_byte(buffer, i + 1)?;
-	        let c = get_byte(buffer, i + 2)?;
-	        let d = get_byte(buffer, i + 3)?;
-	        Ok(((a & 0x07) << 18 | b << 12 | c << 6 | d, 3))
+	        let b = expect_byte(buffer, i + 1)?;
+	        let c = expect_byte(buffer, i + 2)?;
+	        let d = expect_byte(buffer, i + 3)?;
+	        ((a & 0x07) << 18 | b << 12 | c << 6 | d, 3)
 		} else {
-			Err(())
-		}
+			return Err(())
+		};
+
+		Ok(Some(r))
 	} else {
-		return None
+		return Ok(None)
 	}
 }
 
